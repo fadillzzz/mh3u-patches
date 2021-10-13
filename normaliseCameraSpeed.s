e73eca626e66adbfbb101f5a7aa992c43ab422f0
Kemal Fadillah <fadillzzz@gmail.com>
2021-10-12 17:16:27 -0700

    fix(hammer): Fixes hammer charge attack distance

--- normaliseCameraSpeed.s
+++ normaliseCameraSpeed.s
@@ -4,9 +4,9 @@
 .global NormaliseCameraSpeed
 NormaliseCameraSpeed:
     fmuls s0, s1, s3
-    ldr r0, =0x8119d90
-    ldr r0, [r0]
-    ldr r0, [r0,#0x14]
+    ldr   r0, =0x8119d90
+    ldr   r0, [r0]
+    ldr   r0, [r0,#0x14]
     fmsr  s4, r0
     mov   r0, #0x40000000
     fmsr  s5, r0
