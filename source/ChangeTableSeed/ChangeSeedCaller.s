e519d5aadd22a18ab7e1de7418e270df3f85dde5
fadillzzz <fadillzzz@gmail.com>
2024-02-18 05:23:25 +0000

    fix(charm): Fixes charm table loading causes an exception (#5)

--- source/ChangeTableSeed/ChangeSeedCaller.s
+++ source/ChangeTableSeed/ChangeSeedCaller.s
@@ -4,9 +4,9 @@
 .global ChangeSeedCaller
 
 ChangeSeedCaller:
-    push {r0, r14}
+    push {r0, r3, r14}
     mov r0, r2
     bl ChangeSeed
     ldrh r2, [r2] // Original code
-    pop {r0, r14}
+    pop {r0, r3, r14}
     bx lr
